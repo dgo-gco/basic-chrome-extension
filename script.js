2edc307491617cbe01d15ea78cb735a2f45bbd6c
Diego <110729317+dgo-gco@users.noreply.github.com>
2022-11-04 22:56:21 +0100

    adding localStorage funcionality

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-const inputEl = document.getElementById('input-el');
+
 let myLeads = [];
 const inputBtn = document.getElementById('input-btn');
+const inputEl = document.getElementById('input-el');
 const ulEl = document.getElementById('ul-el');
+
+let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+
+if (leadsFromLocalStorage) { //checking if it's a truthy value 
+    myLeads = leadsFromLocalStorage;
+    renderLeads();
+};
 
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value);
     renderLeads();
     inputEl.value = '';
+    localStorage.setItem("myLeads", JSON.stringify(myLeads)) //After .setItem we need TWO strings, so we conver our variable to string by stringifying it
 });
 
 function renderLeads() {
