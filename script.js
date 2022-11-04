798d37ca386d9beee0c9270f8e91d78b9857fe73
Diego <110729317+dgo-gco@users.noreply.github.com>
2022-11-04 15:43:07 +0100

    adding files

---
+++ script.js
@@ -0,0 +1,31 @@
+const inputEl = document.getElementById('input-el');
+let myLeads = [];
+const inputBtn = document.getElementById('input-btn');
+const ulEl = document.getElementById('ul-el');
+
+inputBtn.addEventListener("click", function(){
+    myLeads.push(inputEl.value);
+    renderLeads();
+    inputEl.value = '';
+});
+
+function renderLeads() {
+    let listItems = '';
+
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
+    };
+    ulEl.innerHTML = listItems; 
+}
+
+
+
+
+
+
